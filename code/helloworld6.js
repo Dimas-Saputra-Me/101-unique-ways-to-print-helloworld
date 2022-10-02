e15699efa2b1d3909c2841627efaca3c91217380
Sahil Bhilave <sahilbhilave@gmail.com>
2022-10-02 12:50:47 +0530

    Added a new way to print hello world
    Converted the ASCII values to char to print 'Hello World'

---
+++ code/helloworld6.js
@@ -0,0 +1,9 @@
+// TEMPLATE
+// Title  : [Hello World Using ASCII] 
+// Author: [Sahil-Bhilave](https://github.com/sahilbhilave)
+
+function print(){
+    var chars = String.fromCharCode(72,69,76,76,79,32,87,79,82,76,68);
+    console.log(chars);
+}
+print();
