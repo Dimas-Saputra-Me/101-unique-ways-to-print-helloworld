1c4c57ff5fd77af5f2794e0d925de4a6fddb4340
Madhurmoms <70679523+Madhur-Jatiya@users.noreply.github.com>
2022-10-15 13:39:25 +0530

    Create WaysToPrintHello.java

---
+++ java/WaysToPrintHello.java
@@ -0,0 +1,15 @@
+Different ways to Print Hello World using java
+// Title  : [Call hello world using another method]
+// Contributor: [Madhur Jatiya](https://github.com/Madhur-Jatiya  ------ https://madhurmoms.me/)
+
+public class WaysToPrintHello {
+    WaysToPrintHello()
+    {
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        WaysToPrintHello obj1 = new WaysToPrintHello();
+
+    }
+}
