ce43d2eec7fb6b768a45ee86433d89a082cf3c31
Dimas Saputra <62497214+Dimas-Saputra-Me@users.noreply.github.com>
2022-10-15 19:39:38 +0700

    Removing unnecessary line

--- java/WaysToPrintHello.java
+++ java/WaysToPrintHello.java
@@ -1,4 +1,3 @@
-Different ways to Print Hello World using java
 // Title  : [Call hello world using another method]
 // Contributor: [Madhur Jatiya](https://github.com/Madhur-Jatiya  ------ https://madhurmoms.me/)
 
