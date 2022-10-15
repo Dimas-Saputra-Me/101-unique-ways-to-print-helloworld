36223d5dc31a88d0d1e6e6d9a15eff2bcd5e98d4
Madhurmoms <70679523+Madhur-Jatiya@users.noreply.github.com>
2022-10-15 21:08:05 +0530

    Create anotherWayToPrintHelloWorld.java

---
+++ java/anotherWayToPrintHelloWorld.java
@@ -0,0 +1,11 @@
+public class WaysToPrintHello {
+    WaysToPrintHello(String s)
+    {
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        WaysToPrintHello obj1 = new WaysToPrintHello("This is my third way of program");
+
+    }
+}
